a7cc548a899fc3d9c06a66ccedaa25d82d9d8d25
Dichi Al Faridi <dichi.alfaridi@gmail.com>
2015-08-13 03:09:45 +0700

    simple 3D secure example with vtandroidlib

--- app/src/main/java/info/alfaridi/webview/MainActivity.java
+++ app/src/main/java/info/alfaridi/webview/MainActivity.java
@@ -4,8 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 import org.xwalk.core.XWalkView;
+
+import id.co.veritrans.android.api.VTDirect;
+import id.co.veritrans.android.api.VTInterface.ITokenCallback;
+import id.co.veritrans.android.api.VTModel.VTCardDetails;
+import id.co.veritrans.android.api.VTModel.VTToken;
+import id.co.veritrans.android.api.VTUtil.VTConfig;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +23,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        XWalkView walkView = (XWalkView) findViewById(R.id.vtwebview);
-        walkView.setPadding(0, 0, 0, 0);
-        walkView.load("https://bbmpay.veritrans.co.id/merchant/#/login", null);
+        VTDirect vtDirect = new VTDirect();
+        VTConfig.CLIENT_KEY = "";
+        VTConfig.VT_IsProduction = true;
+
+        VTCardDetails cardDetails = new VTCardDetails();
+        cardDetails.setCard_number("");
+        cardDetails.setCard_cvv("");
+        cardDetails.setCard_exp_month(3);
+        cardDetails.setCard_exp_year(2019);
+        cardDetails.setSecure(true);
+        cardDetails.setGross_amount(50000 + "");
+
+        vtDirect.setCard_details(cardDetails);
+
+        vtDirect.getToken(new ITokenCallback() {
+            @Override
+            public void onSuccess(VTToken vtToken) {
+                if(vtToken.getRedirect_url() != null) {
+                    XWalkView webview = (XWalkView) findViewById(R.id.vtwebview);
+                    webview.setPadding(0, 0, 0, 0);
+                    webview.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent motionEvent) {
+                            switch (motionEvent.getAction()) {
+                                case MotionEvent.ACTION_DOWN:
+                                case MotionEvent.ACTION_UP:
+                                    if (!v.hasFocus()) {
+                                        v.requestFocus();
+                                    }
+                                    break;
+                            }
+
+                            return false;
+                        }
+                    });
+
+                    webview.load(vtToken.getRedirect_url(), null);
+                }
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
