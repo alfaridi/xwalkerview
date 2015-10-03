2cf8aead30e86444f359f11f58e1946c37250b38
Dichi Al Faridi <dichi.alfaridi@gmail.com>
2015-10-03 09:26:28 +0700

    handle if 3ds step is complete

--- app/src/main/java/info/alfaridi/webview/MainActivity.java
+++ app/src/main/java/info/alfaridi/webview/MainActivity.java
@@ -1,5 +1,6 @@
 package info.alfaridi.webview;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,6 +9,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
+import org.xwalk.core.XWalkResourceClient;
 import org.xwalk.core.XWalkView;
 
 import id.co.veritrans.android.api.VTDirect;
@@ -18,18 +20,25 @@ import id.co.veritrans.android.api.VTUtil.VTConfig;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static Context context;
+
+    public static Context getContext() {
+        return context;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = getApplicationContext();
         setContentView(R.layout.activity_main);
 
         VTDirect vtDirect = new VTDirect();
-        VTConfig.CLIENT_KEY = "";
-        VTConfig.VT_IsProduction = true;
+        VTConfig.CLIENT_KEY = "VT-client-SimkwEjR3_fKj73D";
+        VTConfig.VT_IsProduction = false;
 
         VTCardDetails cardDetails = new VTCardDetails();
-        cardDetails.setCard_number("");
-        cardDetails.setCard_cvv("");
+        cardDetails.setCard_number("4811111111111114");
+        cardDetails.setCard_cvv("123");
         cardDetails.setCard_exp_month(3);
         cardDetails.setCard_exp_year(2019);
         cardDetails.setSecure(true);
@@ -42,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(VTToken vtToken) {
                 if(vtToken.getRedirect_url() != null) {
                     XWalkView webview = (XWalkView) findViewById(R.id.vtwebview);
+                    webview.setResourceClient(new VtXWalkResource(webview));
                     webview.setPadding(0, 0, 0, 0);
                     webview.setOnTouchListener(new View.OnTouchListener() {
                         @Override
@@ -70,5 +80,26 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private class VtXWalkResource extends XWalkResourceClient {
+
+        public VtXWalkResource(XWalkView view) {
+            super(view);
+        }
+
+        @Override
+        public void onLoadStarted(XWalkView view, String url) {
+            super.onLoadStarted(view, url);
+        }
+
+        @Override
+        public void onLoadFinished(XWalkView view, String url) {
+            super.onLoadFinished(view, url);
+            if(url.startsWith("https://api.sandbox.veritrans.co.id/v2/token/callback")) {
+                Toast.makeText(MainActivity.getContext(), "Callback success, continue to charging.", Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 }
